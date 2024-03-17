0a20204d76d99183c7629bb07135d0f4c6c38cb9
mamulaidze <mamulaidzeg186@gmail.com>
2024-03-17 18:14:01 +0400

    project 2

---
+++ JavaScript/project 0.2/index.js
@@ -0,0 +1,14 @@
+alert("pentagoni")
+
+function Smaller(){
+    document.getElementById('foto').width = '100';
+}
+function Bigger(){
+    document.getElementById('foto').width = '500'
+}
+
+let num1 = 3;
+let num2 = 87;
+let num3 = 97;
+
+console.log(num3 / (num2 - num1))
