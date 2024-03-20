8359f1e08fc5f8f6f5e898a58281ee0bf588f9b4
mamulaidze <mamulaidzeg186@gmail.com>
2024-03-20 21:17:11 +0400

    project 2

--- JavaScript/project 0.2/index.js
+++ JavaScript/project 0.2/index.js
@@ -6,7 +6,9 @@ function Smaller(){
 function Bigger(){
     document.getElementById('foto').width = '500'
 }
-
+function Default(){
+    document.getElementById('foto').width = '300'
+}
 let num1 = 3;
 let num2 = 87;
 let num3 = 97;
