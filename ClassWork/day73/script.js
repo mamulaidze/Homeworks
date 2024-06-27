1787096a0ddd45c0680eb9af50aa621aa47ef277
mamulaidze <mamulaidzeg186@gmail.com>
2024-06-27 22:50:31 +0400

    day 73

--- ClassWork/day73/script.js
+++ ClassWork/day73/script.js
@@ -5,3 +5,28 @@ cars.pop()
 console.log(cars)
 cars[2] = "Toyota"
 console.log(cars)
+    //2
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let sum = 0
+for (let i = 0; i < nums.length; i++){
+        sum += nums[i]
+}
+console.log("the sum is: ", sum)
+
+    //3
+const names = new Array("Gio" , "Gio","Gio" , "nika" , "ilia", "dachi", "dima", "edi", "ucha")
+let saxeli = "Gio"
+let saxelebisJami = 0
+for (let i = 0; i < names.length; i++) {
+        if (names[i] === saxeli) {
+            saxelebisJami++;
+    }
+}
+console.log(saxeli,"meordeba",saxelebisJami+"-jer")
+
+    //4
+const cifrebi = new Array(1,2,3,4,5,6,7,8,9,10)
+let oddOrEven
+for(i=0;i<cifrebi.length; i++){
+    for
+}
